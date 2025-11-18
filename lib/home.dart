4893abaa3489cb89ac294cb395461f3e5cf4c400
vitor <mcentregas@gmail.com>
2025-11-17 22:01:07 -0300

    abastecimento

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,3 @@
-import 'package:atividadefinal/src/paginas/abastecimento_list.dart';
-import 'package:atividadefinal/src/paginas/veiculos_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
